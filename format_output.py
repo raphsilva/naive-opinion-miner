d7d879c0b2fa9fb56fc9169c02f9493187cebb20
raphsilva <raphaelsilva2500@gmail.com>
2019-09-14 10:14:47 -0300

    Removed counting of single-aspect opinions

--- format_output.py
+++ format_output.py
@@ -19,7 +19,6 @@ def make_overview_table(data_to_write):
     s += "%10s" % 'neutral'
     s += "%10s" % 'non-op'
     s += "%10s" % 'total'
-    s += "%10s" % 'exclus'
     s = s.replace(' ', '_')
     s += '\n'
     for a in sorted(list_of_aspects, key=unikey):
@@ -36,17 +35,13 @@ def make_overview_table(data_to_write):
 
 def count_aspects(info):
     count_aspects = {}
-    count_aspects['_TOTAL_'] = {'+': 0, '-': 0, '.': 0, 'x': 0, 'total': 0, 'exc': 0}
+    count_aspects['_TOTAL_'] = {'+': 0, '-': 0, '.': 0, 'x': 0, 'total': 0}
     for i in info:
         for asp in i['aspects']:
             if asp not in count_aspects:
-                count_aspects[asp] = {'+': 0, '-': 0, '.': 0, 'x': 0, 'total': 0, 'exc': 0}
+                count_aspects[asp] = {'+': 0, '-': 0, '.': 0, 'x': 0, 'total': 0}
             count_aspects[asp][(i['polarity'])] += 1
             count_aspects['_TOTAL_'][(i['polarity'])] += 1
-
-            if len(i['aspects']) == 1:
-                count_aspects[asp]['exc'] += 1
-                count_aspects['_TOTAL_']['exc'] += 1
 
     return count_aspects
 
@@ -59,5 +54,4 @@ def aspect_stats(list_of_aspects, aspect):
     s += "%10d" % list_of_aspects[aspect]['.']
     s += "%10d" % list_of_aspects[aspect]['x']
     s += "%10d" % (list_of_aspects[aspect]['+'] + list_of_aspects[aspect]['-'] + list_of_aspects[aspect]['.'] + list_of_aspects[aspect]['x'])
-    s += "%10d" % (list_of_aspects[aspect]['exc'])
     return s.replace(' 0 ', '   ')
